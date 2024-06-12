494732524d85ef882cd3ec662a789bc08a2fd4f6
Smriti Doneria <smritidoneria@gmail.com>
2024-06-12 10:16:27 +0530

    done with trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,7 +5,11 @@ from src.exception import CustomException
 from src.logger import logging
 from sklearn.model_selection import train_test_split    
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
+from src.components.model_trainer import ModelTainerConfig
+from src.components.model_trainer import ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
@@ -41,6 +45,12 @@ class DataIngestion:
             raise CustomException(e,sys)
 
 if __name__=="__main__":
-    data_ingestion=DataIngestion()
-    data_ingestion.initiate_data_ingestion()
+    obj=DataIngestion()
+    train_data,test_data,raw_data=obj.initiate_data_ingestion()
+    data_transformation_obj=DataTransformation()
+    train_arr, test_arr,_=data_transformation_obj.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
+
 
